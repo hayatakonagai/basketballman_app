e1f531fcfe9d9c70cdf3b18505235a33fe9a5677
hayatakonagai <scchr1208@gmail.com>
2020-10-03 16:48:06 +0900

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Category;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->category !== null) {
+            $products = Product::where('category_id', $request->category)->paginate(15);
+            $category = Category::find($request->category);
+        } else {
+            $products = Product::paginate(15);
+            $category = null;
+        }
+
+        $categories = Category::all();
+        $major_category_names = Category::pluck('major_category_name')->unique();
+
+        return view('products.index', compact('products', 'category', 'categories', 'major_category_names'));
+    }
+        //
+    
+        public function favorite(Product $product)
+        {
+            $user = Auth::user();
+    
+            if ($user->hasFavorited($product)) {
+                $user->unfavorite($product);
+            } else {
+                $user->favorite($product);
+            }
+    
+            return redirect()->route('products.show', $product);
+        }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('products.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->category_id = $request->input('category_id');
+        $product->save();        
+        return redirect()->route('products.show', ['id' => $product->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+     $reviews = $product->reviews()->get();
+     return view('products.show', compact('product', 'reviews'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        $categories = Category::all();
+         return view('products.edit', compact('product', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+         $product->name = $request->input('name');
+         $product->description = $request->input('description');
+         $product->price = $request->input('price');
+         $product->category_id = $request->input('category_id');
+         $product->update();
+         return redirect()->route('products.show', ['id' => $product->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+       $product->delete();
+        return redirect()->route('products.index');
+    }
+}
---
+++ database/migrations/2020_10_03_060226_create_teams_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('status',['募集中','応募終了']);
+            $table->string('name',255);
+            $table->enum('level',['初級','中級','上級']);
+            $table->string('goal',255);
+            $table->enum('where',['北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県']);
+            $table->string('where_city',255);
+            $table->string('where_detail',255);
+            $table->enum('frequency',['週1','週2','週3','週4','週5','週6','週7']);
+            $table->string('people',255);
+            $table->text('wanted');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
