19e0fc2dfb64beb7b4e58cd4833ed213e5d6528e
hayatakonagai <scchr1208@gmail.com>
2020-11-03 12:23:55 +0900

    update

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -18,14 +18,14 @@
     </div>
 --}}
 
-    @auth
+    
     <div class="row justify-content-center">
         <div class="col-md-5">
             <h3 class="mt-3 mb-3"><a href="{{route('teams.create')}}">新規メンバー募集</a></h3>
             <hr>
         </div>
     </div>
-    @endauth
+    
     <div class="row justify-content-center">
         <div class="col-md-5">
             @foreach($teams as $team)
