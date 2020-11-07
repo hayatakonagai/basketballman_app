fd41a588b6a9b60ba9f7ad168812d1e0777b5c9f
hayatakonagai <scchr1208@gmail.com>
2020-11-07 16:08:43 +0900

    fix to deploy20201105

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -36,14 +36,14 @@
                         <dt>活動場所</dt><dd>{{$team->where}}</dd>
                         <dt>活動頻度</dt><dd>{{$team->frequency}}</dd>
                         <dt>更新日</dt><dd>{{$team->updated_at}}</dd>
-                        <td>
-                        @if (config('const.env') == "local" && $team->image !== "")
-                            <img src="{{ asset('storage/team/'.$team->image) }}" class="h-10 img-fluid">
-                        @endif
-                        @if (config('const.env') == "production" && $team->image !== "")
-                        <img src="{{ Storage::disk('s3')->url($team->image) }}" class="h-10 img-fluid">
-                        @endif
-                    </td>
+                        <div class ="team-index-img">
+                            @if (config('const.env') == "local" && $team->image !== "")
+                                <img src="{{ asset('storage/team/'.$team->image) }}" class="h-10 img-fluid" style=width:150px>
+                            @endif
+                            @if (config('const.env') == "production" && $team->image !== "")
+                                <img src="{{ Storage::disk('s3')->url($team->image) }}" class="h-10 img-fluid" style=width:150px>
+                            @endif
+                        </div>
                 </div>
             @endforeach
             {{ $teams->links() }}
