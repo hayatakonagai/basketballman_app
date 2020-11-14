a0fb2dd35e2be7eec71d7f52ddd8fc57618ccc95
hayatakonagai <scchr1208@gmail.com>
2020-11-14 16:11:43 +0900

    fix to deploy20201114

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
+use App\Team;
+
 
 class WebController extends Controller
 {
     public function index()
-    {
-        return view('web.index');
+    {   
+        $teams = Team::orderBy('updated_at', 'desc')->paginate(10);
+        return view('web.index',compact('teams'));
     }
 }
--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-2">
-    </div>
-    <div class="col-9">
-        <h1>{{config('app.name')}}</h1>
-        <div class="row">
-            <div class="col-4">
-                <a href="#">
-                    <img src="{{ asset('img/orange.png') }}" class="img-thumbnail">
-                </a>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="samazon-product-label mt-2">
-                            旬のオレンジ詰め合わせ<br>
-                            <label>￥2000</label>
-                        </p>
-                    </div>
+<div class="row justify-content-center">
+        <div class="col-md-5 col-xs-10">
+            @foreach($teams as $team)
+                <div class="main-list">
+                    <dl class="teams-index">
+                        <dt>チーム名</dt><dd><a href="{{route('teams.show',['id'=>$team->id])}}">{{$team->name}}</a></dd>
+                        <dt>応募資格</dt><dd>{{$team->wanted}}</dd>
+                        <dt>活動場所</dt><dd>{{$team->where}}</dd>
+                        <dt>活動頻度</dt><dd>{{$team->frequency}}</dd>
+                        <dt>更新日</dt><dd>{{$team->updated_at}}</dd>
+                        <div class ="team-index-img">
+                            @if (config('const.env') == "local" && $team->image !== "")
+                                <img src="{{ asset('storage/team/'.$team->image) }}"style=width:150px>
+                            @endif
+                            @if (config('const.env') == "production" && $team->image !== "")
+                                <img src="{{ Storage::disk('s3')->url($team->image) }}" style=width:150px>
+                            @endif
+                        </div>
                 </div>
-            </div>
+            @endforeach
+            {{ $teams->links() }}
         </div>
     </div>
-</div>
 @endsection
