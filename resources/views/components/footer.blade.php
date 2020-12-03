24f03c79fe4dc801f5e16c7d3fd17634120be493
hayatakonagai <scchr1208@gmail.com>
2020-12-03 11:28:10 +0900

    gitの練習
    diffの使い方

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-bottom mt-5">
     <a class="navbar-brand mx-auto" href="{{ url('/') }}">
-    <i class="fas fa-home">HOME</i>
+    <i class="fas fa-home">HOME(git)</i>
     </a>
 </nav>
