f4b26a62d099c08df6b6b480c8c7e2a27412a3c3
hayatakonagai <scchr1208@gmail.com>
2020-12-18 10:39:55 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/login/guest', 'Auth\LoginController@guestLogin');
+
+Route::resource('posts', 'PostsController');
