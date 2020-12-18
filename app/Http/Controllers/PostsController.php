44ca599ffa42570bdaf153d861f56e12790b06ee
hayatakonagai <scchr1208@gmail.com>
2020-12-18 10:41:54 +0900

    Postコントローラー作成

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('posts.index',compact('posts'));
+    }
+
+    public function create()
+    {
+        $user = Auth::user();
+        return view(('posts.create'),compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $params = $request->validate([
+            'title' => 'required|max:50',
+            'category' =>'required',
+            'body' => 'required|max:2000',
+            'url' =>'nullable',
+        ]);
+
+        $post= new Post;
+        $user = Auth::user();
+        $post->title =$request->title;
+        $post->category =$request->category;
+        $post->body =$request->body;
+        $post->url =$request->url;
+        $post->user_id = $user->id;
+
+        $post->save();
+        return redirect()->route('posts.index');
+    }
+
+    public function show(Post $post , User $user)
+    {
+        $user = User::all();
+        return view('posts.show',compact('post','user'));
+    }
+
+}
