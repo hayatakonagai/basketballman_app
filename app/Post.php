45e54e4bad41118158df56e2071ea71c4868cddb
hayatakonagai <scchr1208@gmail.com>
2020-12-18 10:38:18 +0900

    Postモデル、Commentモデル作成

---
+++ app/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'id','title','category','body','url'
+    ];
+
+    public function user()
+    {
+        return $this->BelongsTo('App\User');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+    
+}
