bb07db61877367ce043c8d158b16cd5970e86111
hayatakonagai <scchr1208@gmail.com>
2020-12-25 10:45:20 +0900

    Likeテーブル・モデル作成

---
+++ app/Like.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+     //いいねしている投稿
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    //いいねが既にされているかを確認
+    public function like_exist($id, $post_id)
+    {
+       //Likesテーブルのレコードにユーザーidと投稿idが一致するものを取得
+        $exist = Like::where('user_id', '=', $id)->where('post_id', '=', $post_id)->get();
+
+        // レコード（$exist）が存在するなら
+        if (!$exist->isEmpty()) {
+            return true;
+        } else {
+        // レコード（$exist）が存在しないなら
+            return false;
+        }
+    }
+}
