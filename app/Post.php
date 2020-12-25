0f707683aa7d4fa6a5acb3db3e3300737e794c3d
hayatakonagai <scchr1208@gmail.com>
2020-12-25 10:44:42 +0900

    Likeテーブルとのリレーション追加

--- app/Post.php
+++ app/Post.php
@@ -19,5 +19,8 @@ class Post extends Model
     {
         return $this->hasMany('App\Comment');
     }
-    
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
